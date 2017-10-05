77a25d362c29ef1de057701182d91d31d187165b
Anbarasi U <anbarasiu@gmail.com>
2017-10-05 11:17:00 +0530

    Revert "HTTP/2"
    This reverts commit 4d6c8b49897f7e5cfdb68ba17c7cc759e0384f07.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express'),
-    spdy = require('spdy'),
-    fs = require('fs'),
     compression = require('compression'),
     path = require('path'),
     jade = require('jade'),
     coffee = require('./routes/coffee'),
     pizza = require('./routes/pizza');
-var port = 8080;
+
 var app = new express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -20,19 +18,4 @@ app.get('/',function(req,res){
   res.render('layout', { title: 'Node.js / Google Maps Example', subtitle: 'with the help of the Express, Path, and Jade modules' });
 });
 
-// app.listen(process.env.PORT || 8080)
-var options = {
-  key: fs.readFileSync(__dirname + '/server.key'),
-  cert:  fs.readFileSync(__dirname + '/server.crt')
-};
-
-spdy
-.createServer(options, app)
-.listen(process.env.PORT || port, (error) => {
-  if (error) {
-    console.error(error)
-    return process.exit(1)
-  } else {
-    console.log('Listening on port: ' + port + '.')
-  }
-});
+app.listen(process.env.PORT || 8080)
